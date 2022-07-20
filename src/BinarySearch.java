fe9eae4535f0dd5d231bf59b77ea02b5cebc4d3d
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-07-20 11:08:16 +0300

    Update BinarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{-1, 3, 5, 8, 10, 15, 16, 20}, 7));
+
+    }
+
+    public static int binarySearch(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+
+            if (key < a[middle]) {
+                high = middle - 1;
+            } else if (key > a[middle]) {
+                low = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
 }
